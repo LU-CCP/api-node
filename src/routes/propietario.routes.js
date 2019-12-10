414a5618304110070074ee0d483210ab37d6f563
Alfredo Puerta <56939672+alfredop-lagash@users.noreply.github.com>
2019-12-10 18:30:53 -0300

    Ejercicio completo funcional

--- src/routes/propietario.routes.js
+++ src/routes/propietario.routes.js
@@ -1,29 +1,54 @@
-const express = require('express');
-const sql = require('mssql');
-const sqlConfig = require('../mssqlConfig');
+const express = require("express");
+const sql = require("mssql");
+const sqlConfig = require("../mssqlConfig");
 
 const router = express.Router();
 
 //Crear una ruta que permita registrar un propietario
 
+router.get("/", function(req, res) {
+  sql.connect(sqlConfig.config, function(err) {
+    if (err) console.log(err);
 
-router.get('/', function (req, res) {
-    sql.connect(sqlConfig.config, function (err) {
+    const sqlRequest = new sql.Request();
 
-        if (err) console.log(err);
+    sqlRequest.query("select * from persona", function(error, data) {
+      if (error) console.log(error);
 
-        const sqlRequest = new sql.Request();
+      res.send(data);
 
-        sqlRequest.query('select * from persona', function (error, data) {
-
-            if (error) console.log(error);
-
-            res.send(data);
-
-            sql.close();
-        })
+      sql.close();
     });
+  });
+});
 
+router.post("/add", function(req, res) {
+  const {
+    rut,
+    nombre,
+    apellido_materno,
+    apellido_paterno,
+    telefono
+  } = req.body;
+  sql.connect(sqlConfig.config, function(err) {
+    if (err) console.log(err);
+
+    const sqlRequest = new sql.Request();
+    sqlRequest.input("rut", rut);
+    sqlRequest.input("nombre", nombre);
+    sqlRequest.input("apellido_materno", apellido_materno);
+    sqlRequest.input("apellido_paterno", apellido_paterno);
+    sqlRequest.input("telefono", telefono);
+    sqlRequest.query(
+      "INSERT INTO Persona VALUES(@rut, @nombre, @apellido_materno, @apellido_paterno, @telefono)",
+      function(error, data) {
+        if (error) console.log(error);
+
+        res.send(data);
+        sql.close();
+      }
+    );
+  });
 });
 
 module.exports = router;
