79eef5f89198583c5daeb324943ea4ab7e647d5e
nicolasar-lagash <nicolasar@lagash.com>
2019-12-10 17:59:46 -0300

    Agregar propietario

--- src/routes/propietario.routes.js
+++ src/routes/propietario.routes.js
@@ -1,29 +1,55 @@
-const express = require('express');
-const sql = require('mssql');
-const sqlConfig = require('../mssqlConfig');
+const express = require("express");
+const sql = require("mssql");
+const sqlConfig = require("../mssqlConfig");
 
 const router = express.Router();
 
 //Crear una ruta que permita registrar un propietario
 
+router.get("/", function(req, res) {
+  sql.connect(sqlConfig.config, function(err) {
+    if (err) console.log(err);
 
-router.get('/', function (req, res) {
-    sql.connect(sqlConfig.config, function (err) {
+    const sqlRequest = new sql.Request();
 
-        if (err) console.log(err);
+    sqlRequest.query("select * from persona", function(error, data) {
+      if (error) console.log(error);
 
-        const sqlRequest = new sql.Request();
+      res.send(data);
 
-        sqlRequest.query('select * from persona', function (error, data) {
-
-            if (error) console.log(error);
-
-            res.send(data);
-
-            sql.close();
-        })
+      sql.close();
     });
+  });
+});
 
+router.post("/agregar", function(request, response) {
+  const {
+    rut,
+    nombre,
+    apellido_materno,
+    apellido_paterno,
+    telefono
+  } = request.body;
+
+  sql.connect(sqlConfig.config, function(error) {
+    if (error) {
+      console.log(error);
+    } else {
+      const sqlRequest = new sql.Request();
+
+      sqlRequest.query(
+        `INSERT INTO Persona VALUES('${rut}', '${nombre}', '${apellido_materno}', '${apellido_paterno}', '${telefono}')`,
+        function(error, data) {
+          if (error) {
+            console.log(error);
+          } else {
+            response.send(data.recordsets);
+            sql.close();
+          }
+        }
+      );
+    }
+  });
 });
 
 module.exports = router;
