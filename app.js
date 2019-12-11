db1e031cfbbbf38558cac11b561336bc7e5fa2cf
fabianb-lagash <fabianb@lagash.com>
2019-12-11 17:41:49 -0300

    Se agrega api node

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
-const express = require("express");
-const propietarioRoutes = require("./src/routes/propietario.routes");
-const pacienteRoutes = require("./src/routes/paciente.routes");
-const registrarPropietario = require("./src/routes/propietarioRegistrar.routes");
-const registrartCita = require("./src/routes/citas.routes");
+const express = require('express');
+const propietarioRoutes = require('./src/routes/propietario.routes');
+const pacienteRoutes = require('./src/routes/paciente.routes');
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 const app = express();
 
-app.use(express.json());
-app.use("/propietario", propietarioRoutes);
-app.use("/paciente", pacienteRoutes);
-app.use("/registrarpropietario", registrarPropietario);
-app.use("/registrarcita", registrartCita);
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'LU API',
+            description: 'API Creada por LU',
+            servers: ['http://localhost:3005'],
+        },
+    },
+    apis: [`${process.cwd()}/src/routes/*.js`]
+}
 
-app.listen(3002, function() {
-  console.log("Server running on port 3002");
-});
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use(express.json());
+app.use('/propietario', propietarioRoutes);
+app.use('/paciente', pacienteRoutes);
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /test:
+ *  get:
+ *      description: Utilizado a modo de prueba para testear el swagger
+ *      responses:
+ *          '200':
+ *              description: Respuesta exitosa!
+ *          '404':
+ *              description: Recurso no encontrado
+ */
+app.get('/test', function (req, res) {
+    res.send('test');
+})
+
+app.listen(3005, function () {
+    console.log('Server running on port 3005');
+})
--- src/mssqlConfig.js
+++ src/mssqlConfig.js
@@ -1,12 +1,13 @@
 const dbConfig = {
-  user: "magister",
-  password: "A7r9bFRTBtyXot7IPxin",
-  server: "magisterdereemplazo.database.windows.net",
-  database: "magisterdereemplazo",
-  port: 1433,
-  options: {
-    encrypt: true
-  }
-};
+    user: 'magister',
+    password: 'A7r9bFRTBtyXot7IPxin',
+    server: 'magisterdereemplazo.database.windows.net',
+    database: 'magisterdereemplazo',
+    port: 1433,
+    options:
+    {
+        encrypt: true
+    }
+}
 
 exports.config = dbConfig;
