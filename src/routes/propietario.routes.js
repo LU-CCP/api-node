5d24849475b53e19fe7cce9558dba9a51c58c969
nicolasar-lagash <nicolasar@lagash.com>
2019-12-11 17:40:58 -0300

    arreglado

--- src/routes/propietario.routes.js
+++ src/routes/propietario.routes.js
@@ -16,65 +16,46 @@ const router = express.Router();
  *          '404':
  *              description: Recurso no encontrado
  */
-router.get('/test', function (req, res) {
-
-<<<<<<< HEAD
-router.get("/", function(req, res) {
+router.get("/test", function(req, res) {
   sql.connect(sqlConfig.config, function(err) {
     if (err) console.log(err);
-
     const sqlRequest = new sql.Request();
 
     sqlRequest.query("select * from persona", function(error, data) {
       if (error) console.log(error);
-
       res.send(data);
-
       sql.close();
     });
   });
 });
-=======
-    sql.connect(sqlConfig.config, function (err) {
-        if (err) console.log(err);
-        const sqlRequest = new sql.Request();
-
-        sqlRequest.query('select * from persona', function (error, data) {
-            if (error) console.log(error);
-            res.send(data);
-            sql.close();
-        })
-    });
-});
 
 //promises
-router.get('/promise', function (req, res) {
-    sql.connect(sqlConfig.config)
-        .then(function (conn) {
-            return conn.query('select * from persona');
-        })
-        .then(data => {
-            res.send(data);
-        })
-        .catch(error => console.log(error))
+router.get("/promise", function(req, res) {
+  sql
+    .connect(sqlConfig.config)
+    .then(function(conn) {
+      return conn.query("select * from persona");
+    })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(error => console.log(error));
 });
 
 //async await
-router.get('/async', async (req, res) => {
+router.get("/async", async (req, res) => {
+  try {
+    let conn = await sql.connect(sqlConfig.config);
 
-    try {
-        let conn = await sql.connect(sqlConfig.config);
+    let result = await conn.request().query("select * from persona");
 
-        let result = await conn.request().query('select * from persona');
+    sql.close();
 
-        sql.close();
-
-        res.send(result);
-    }
-    catch (error) {
-        console.log(error)
-    }
->>>>>>> master
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.post("/agregar", function(request, response) {
   const {
@@ -106,12 +87,4 @@ router.post("/agregar", function(request, response) {
   });
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-//Crear una ruta que permita el registro de las citas,
-//previo a hacer el registro se debe validar que existan los regitros 
-//de idPaciente y idPersona asociados.
-
-module.exports = router;
->>>>>>> master
