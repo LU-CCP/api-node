d5b808057a30f0b6255f5158a98c8e8491ad8a69
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-12-11 17:38:44 -0300

    Rutas 11/12

--- src/routes/paciente.routes.js
+++ src/routes/paciente.routes.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const dbConfig = require("../mssqlConfig");
+const sql = require("mssql");
 
-router.get('/', function (req, res) {
-    res.send('ruta get paciente');
+router.get("/", function(req, res) {
+  sql.connect(dbConfig.config, function(err) {
+    if (err) console.log(err);
+    const sqlRequest = new sql.Request();
+    sqlRequest.query("select * from paciente", function(error, data) {
+      if (error) console.log(error);
+      res.send(data);
+      sql.close();
+    });
+  });
 });
 
 module.exports = router;
--- src/routes/propietario.routes.js
+++ src/routes/propietario.routes.js
@@ -1,29 +1,45 @@
-const express = require('express');
-const sql = require('mssql');
-const sqlConfig = require('../mssqlConfig');
-
+const express = require("express");
+const sql = require("mssql");
 const router = express.Router();
+const dbConfig = require("../mssqlConfig");
+const server = express();
+server.use(express.json());
 
-//Crear una ruta que permita registrar un propietario
+router.post("/agregar", function(req, res) {
+  console.log(req.params);
+  console.log(req.body);
+  const body = req.body;
+  const { rut, nombre, apellido_materno, apellido_paterno, telefono } = body;
+  sql.connect(dbConfig.config, function(err) {
+    if (err) console.log(err);
+    const sqlRequest = new sql.Request();
+    sqlRequest.query(
+      `INSERT INTO Persona values('${rut}', '${nombre}', '${apellido_materno}', '${apellido_paterno}', '${telefono}')`,
+      function(error, data) {
+        if (error) console.log(error);
+        res.send("f");
+        sql.close();
+      }
+    );
+  });
+});
 
+module.exports = router;
 
-router.get('/', function (req, res) {
-    sql.connect(sqlConfig.config, function (err) {
+router.get("/", function(req, res) {
+  sql.connect(sqlConfig.config, function(err) {
+    if (err) console.log(err);
 
-        if (err) console.log(err);
+    const sqlRequest = new sql.Request();
 
-        const sqlRequest = new sql.Request();
+    sqlRequest.query("select * from Persona", function(error, data) {
+      if (error) console.log(error);
 
-        sqlRequest.query('select * from persona', function (error, data) {
+      res.send(data);
 
-            if (error) console.log(error);
-
-            res.send(data);
-
-            sql.close();
-        })
+      sql.close();
     });
-
+  });
 });
 
 module.exports = router;
