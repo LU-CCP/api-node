fd3cf069a332cbc1f8a2844c1c81a9d98747a2bc
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-12-11 17:46:20 -0300

    ejercicio

--- src/routes/propietario.routes.js
+++ src/routes/propietario.routes.js
@@ -16,90 +16,89 @@ const router = express.Router();
  *          '404':
  *              description: Recurso no encontrado
  */
-router.get('/test', function (req, res) {
+router.get("/test", function(req, res) {
+  // router.get('/', function (req, res) {
+  //     sql.connect(sqlConfig.config, function (err) {
 
-// router.get('/', function (req, res) {
-//     sql.connect(sqlConfig.config, function (err) {
+  //         if (err) console.log(err);
 
-//         if (err) console.log(err);
+  //         const sqlRequest = new sql.Request();
 
-//         const sqlRequest = new sql.Request();
+  //         sqlRequest.query('select * from persona', function (error, data) {
 
-//         sqlRequest.query('select * from persona', function (error, data) {
+  //             if (error) console.log(error);
 
-//             if (error) console.log(error);
+  //             res.send(data);
 
-//             res.send(data);
+  //             sql.close();
+  //         })
+  //     });
 
-//             sql.close();
-//         })
-//     });
+  // });
 
-// });
+  router.post("/", function(req, res) {
+    const { fecha, id_paciente, motivo_consulta, id_medico, monto } = req.body;
+    sql.connect(sqlConfig.config, function(err) {
+      if (err) console.log(err);
 
-router.post("/", function(req, res) {
-  const { fecha, id_paciente, motivo_consulta, id_medico, monto } = req.body;
-  sql.connect(sqlConfig.config, function(err) {
-    if (err) console.log(err);
+      const sqlRequest = new sql.Request();
 
-    const sqlRequest = new sql.Request();
+      sqlRequest.input("fecha", fecha);
+      sqlRequest.input("id_paciente", id_paciente);
+      sqlRequest.input("motivo_consulta", motivo_consulta);
+      sqlRequest.input("id_medico", id_medico);
+      sqlRequest.input("monto", monto);
 
-    sqlRequest.input("fecha", fecha);
-    sqlRequest.input("id_paciente", id_paciente);
-    sqlRequest.input("motivo_consulta", motivo_consulta);
-    sqlRequest.input("id_medico", id_medico);
-    sqlRequest.input("monto", monto);
+      sqlRequest.query(
+        "select * from paciente where id = @id_paciente",
+        function(error, data) {
+          if (error) console.log(error);
 
-    sqlRequest.query("select * from paciente where id = @id_paciente", function(
-      error,
-      data
-    ) {
-      if (error) console.log(error);
+          res.send(data.recordsets);
 
-      res.send(data.recordsets);
-
-      if (data.recordsets[0] == 0) sql.close();
+          if (data.recordsets[0] == 0) sql.close();
+        }
+      );
     });
-});
 
-//promises
-router.get('/promise', function (req, res) {
-    sql.connect(sqlConfig.config)
-        .then(function (conn) {
-            return conn.query('select * from persona');
+    //promises
+    router.get("/promise", function(req, res) {
+      sql
+        .connect(sqlConfig.config)
+        .then(function(conn) {
+          return conn.query("select * from persona");
         })
         .then(data => {
-            res.send(data);
+          res.send(data);
         })
-        .catch(error => console.log(error))
-});
+        .catch(error => console.log(error));
+    });
 
-//async await
-router.get('/async', async (req, res) => {
-
-    try {
+    //async await
+    router.get("/async", async (req, res) => {
+      try {
         let conn = await sql.connect(sqlConfig.config);
 
-        let result = await conn.request().query('select * from persona');
+        let result = await conn.request().query("select * from persona");
 
         sql.close();
 
         res.send(result);
-    }
-    catch (error) {
-        console.log(error)
-    }
-
-    sqlRequest.query(
-      "insert into cita values(@fecha, @id_paciente,@motivo_consulta,@id_medico,@monto)",
-      function(error, data) {
-        if (error) console.log(error);
-
-        res.send(data.recordsets);
-
-        sql.close();
+      } catch (error) {
+        console.log(error);
       }
-    );
+
+      sqlRequest.query(
+        "insert into cita values(@fecha, @id_paciente,@motivo_consulta,@id_medico,@monto)",
+        function(error, data) {
+          if (error) console.log(error);
+
+          res.send(data.recordsets);
+
+          sql.close();
+        }
+      );
+    });
   });
 });
 
