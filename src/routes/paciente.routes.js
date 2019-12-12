f863ecd22fa49aa7df86db56f5048281dd1383cb
ivettef <56972785+ivettef@users.noreply.github.com>
2019-12-12 15:48:52 -0300

    metodo post para ingreso de pacientes

--- src/routes/paciente.routes.js
+++ src/routes/paciente.routes.js
@@ -1,8 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send('ruta get paciente');
+const {
+  ingresoPaciente,
+  getPropietario
+} = require("../services/paciente.service.js");
+
+/**
+ * @swagger
+ * /paciente:
+ *  post:
+ *      description: Utilizado para ingresar un campo en la base de datos
+ *      responses:
+ *          '201':
+ *              description: Ingreso Exitoso!
+ *          '400':
+ *              description: Recurso no encontrado
+ */
+
+router.post("/", async (req, res) => {
+  const { body } = req;
+  const { id_propietario } = body;
+  if (await getPropietario(id_propietario)) {
+    res.status(400).send({ message: "Error, Propietario not found" });
+  } else {
+    res.status(201).send(await ingresoPaciente(body));
+  }
 });
 
 module.exports = router;
