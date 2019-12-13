c3231a58e1c524687380d2f13f7758aa0edd6d97
leonardbelmar <leonardbelmar@gmail.com>
2019-12-13 16:41:47 -0300

    se agregan package para realizar unit test

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const swaggerConfig = require('./src/swaggerConfig');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 const propietarioRoutes = require('./src/routes/propietario.routes');
 const pacienteRoutes = require('./src/routes/paciente.routes');
+const { API_PORT } = process.env;
 
 const app = express();
 const swaggerDocs = swaggerJsDoc(swaggerConfig.config);
@@ -16,6 +18,8 @@ app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use('/propietario', propietarioRoutes);
 app.use('/paciente', pacienteRoutes);
 
-app.listen(3005, function () {
-    console.log('Server running on port 3005');
+app.listen(API_PORT, function () {
+    console.log('Server running on port', API_PORT);
 })
+
+module.exports = app;
