6ac69ed46aaac8bf915b16a3aac8279812bebe2d
nicolasar-lagash <nicolasar@lagash.com>
2019-12-13 16:11:51 -0300

    insert edit delete

--- src/routes/diagnostico.routes.js
+++ src/routes/diagnostico.routes.js
@@ -1,7 +1,125 @@
 const express = require("express");
-const diagnosticoService = require("../services/diagnostico.service");
-
+const services = require("../services/diagnostico.services");
 const router = express.Router();
+
+/**
+ * @swagger
+ * /diagnostico:
+ *  post:
+ *      parameters:
+ *      - in: body
+ *        name: diagnostico
+ *        description: Agrega un diagnóstico.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - diagnostico
+ *          properties:
+ *            id_cita:
+ *              type: integer
+ *            descripcion:
+ *              type: string
+ *      responses:
+ *          '200':
+ *              description: Respuesta exitosa
+ *          '404':
+ *              description: Recurso no encontrado
+ *          '400':
+ *              description: Request not be understood by the server
+ */
+router.post("/", async (request, response) => {
+  if (await services.encontrarIdCita(request.body.id_cita)) {
+    try {
+      const results = await services.agregar(request.body);
+      response.status(200).send(results);
+    } catch (error) {
+      console.log(error);
+      response.status(404).send("Error 404");
+    }
+  } else {
+    response.status(400).send("Id cita no encontrado");
+  }
+});
+
+/**
+ * @swagger
+ * /diagnostico/{diagnosticoId}:
+ *  put:
+ *      parameters:
+ *      - in: path
+ *        name: diagnosticoId
+ *        type: integer
+ *        required: true
+ *        description: Diagnóstico id.
+ *      - in: body
+ *        name: diagnostico
+ *        description: Editar un diagnóstico.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - diagnostico
+ *          properties:
+ *            id_cita:
+ *              type: integer
+ *            descripcion:
+ *              type: string
+ *      responses:
+ *          '200':
+ *              description: Respuesta exitosa
+ *          '404':
+ *              description: Recurso no encontrado
+ *          '400':
+ *              description: Request not be understood by the server
+ */
+router.put("/:diagnosticoId", async (request, response) => {
+  console.log("id", request.params.diagnosticoId);
+  if (await services.encontrarIdDiagnostico(request.params.diagnosticoId)) {
+    try {
+      const results = await services.editar(
+        request.params.diagnosticoId,
+        request.body
+      );
+      response.status(200).send(results);
+    } catch (error) {
+      console.log(error);
+      response.status(404).send("Error 404");
+    }
+  } else {
+    response.status(400).send("Id diagnóstico no encontrado");
+  }
+});
+
+/**
+ * @swagger
+ * /diagnostico/{diagnosticoId}:
+ *  delete:
+ *      parameters:
+ *      - in: path
+ *        name: diagnosticoId
+ *        type: integer
+ *        required: true
+ *        description: Diagnóstico id.
+ *      responses:
+ *          '200':
+ *              description: Respuesta exitosa
+ *          '404':
+ *              description: Recurso no encontrado
+ *          '400':
+ *              description: Request not be understood by the server
+ */
+router.delete("/:diagnosticoId", async (request, response) => {
+  if (await services.encontrarIdDiagnostico(request.params.diagnosticoId)) {
+    try {
+      const results = await services.eliminar(request.params.diagnosticoId);
+      response.status(200).send(results);
+    } catch (error) {
+      console.log(error);
+      response.status(404).send("Error 404");
+    }
+  } else {
+    response.status(400).send("Id diagnostico no encontrado");
+  }
+});
 
 /**
  * @swagger
@@ -26,9 +144,7 @@ const router = express.Router();
  */
 
 router.get("/filtrar/:id_cita", async (req, res) => {
-  const result = await diagnosticoService.obtenerPacientesParams(
-    req.params.id_cita
-  );
+  const result = await services.obtenerPacientesParams(req.params.id_cita);
   if (result) {
     res.status(200).send(result);
   }
@@ -58,7 +174,7 @@ router.get("/filtrar/:id_cita", async (req, res) => {
  */
 
 router.get("/filtrarId/:id", async (req, res) => {
-  const result = await diagnosticoService.obtenerPacientesId(req.params.id);
+  const result = await services.obtenerPacientesId(req.params.id);
   if (result) {
     res.status(200).send(result);
   }
--- src/services/diagnostico.service.js
+++ src/services/diagnostico.service.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const sql = require("mssql");
 const sqlConfig = require("../mssqlConfig");
 
@@ -35,4 +34,83 @@ async function obtenerPacientesId(id) {
   }
 }
 
-module.exports = { obtenerPacientesParams, obtenerPacientesId };
+const agregar = async body => {
+  const { id_cita, descripcion } = body;
+
+  const connect = await sql.connect(sqlConfig.config);
+  const results = await connect
+    .request()
+    .input("id_cita", id_cita)
+    .input("descripcion", descripcion)
+    .query("INSERT INTO diagnostico VALUES(@id_cita, @descripcion)");
+
+  connect.close();
+
+  return results;
+};
+
+const editar = async (id, body) => {
+  const { id_cita, descripcion } = body;
+
+  const connect = await sql.connect(sqlConfig.config);
+  const editar = await connect
+    .request()
+    .input("id", id)
+    .input("id_cita", id_cita)
+    .input("descripcion", descripcion)
+    .query(
+      "UPDATE diagnostico SET id_cita = @id_cita, descripcion = @descripcion WHERE diagnostico.id = @id"
+    );
+
+  connect.close();
+
+  return editar;
+};
+
+const eliminar = async id => {
+  const connect = await sql.connect(sqlConfig.config);
+  const eliminar = await connect
+    .request()
+    .input("id", id)
+    .query("DELETE FROM diagnostico WHERE diagnostico.id = @id");
+
+  connect.close();
+
+  return eliminar;
+};
+
+const encontrarIdCita = async id => {
+  const connect = await sql.connect(sqlConfig.config);
+  const results = await connect
+    .request()
+    .input("id", id)
+    .query("SELECT c.id FROM cita c WHERE c.id = @id");
+
+  connect.close();
+
+  return results.recordset[0].length != 0;
+};
+
+const encontrarIdDiagnostico = async id => {
+  const connect = await sql.connect(sqlConfig.config);
+  const results = await connect
+    .request()
+    .input("id", id)
+    .query("SELECT d.id FROM diagnostico d WHERE d.id = @id");
+
+  connect.close();
+
+  console.log(results);
+
+  return results.recordset[0].length != 0;
+};
+
+module.exports = {
+  agregar,
+  editar,
+  eliminar,
+  encontrarIdCita,
+  encontrarIdDiagnostico,
+  obtenerPacientesParams,
+  obtenerPacientesId
+};
