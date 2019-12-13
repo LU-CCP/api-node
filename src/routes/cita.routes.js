4ea8e68642538bca5a3f78bbdf1982830fb4e1f5
Alfredo Puerta <56939672+alfredop-lagash@users.noreply.github.com>
2019-12-12 22:56:37 -0300

    Add swagger

--- src/routes/cita.routes.js
+++ src/routes/cita.routes.js
@@ -2,7 +2,24 @@ const express = require("express");
 const router = express.Router();
 const CitaService = require("../services/cita.service");
 
-router.put("/:id", async (req, res) => {
+/**
+ * @swagger
+ * /:id :
+ *  delete:
+ *      description: Ruta para eliminar una cita por su ID unica
+ *      responses:
+ *          '200':
+ *              description: Respuesta exitosa!
+ *          '404':
+ *              description: Recurso no encontrado
+ *           '500':
+ *               description: No se puede eliminar, error no identificado
+ *      consumes:
+ *              'application/json'
+ *      produces:
+ *              'application/json'
+ */
+router.delete("/:id", async (req, res) => {
   const result = await CitaService.checkCita(req.params.id);
   if (!result) {
     res.status(404).json({ error: "No se encuentra la cita" });
@@ -10,7 +27,9 @@ router.put("/:id", async (req, res) => {
   } else {
     let deleteCita = await CitaService.deleteCita(req.params.id);
     if (!deleteCita) {
-      res.status(404).send("No puedes elminiar la cita");
+      res.status(500).json({
+        error: "No puedes elminiar la cita, hubo un problema inesperado"
+      });
       return;
     }
     res.status(200).json({ msg: "Se elimino correctamente tu cita" });
