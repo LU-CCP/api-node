50d8eb5bb59747f853b33ea55c7ac65c25cbfbcc
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-12-13 16:47:55 -0300

    Trabajo grupal avance

--- src/routes/cita.routes.js
+++ src/routes/cita.routes.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = express.Router();
 const citaService = require("../services/cita.service");
 
+/**
+ * @swagger
+ * /{id}:
+ *  get:
+ *      description: ruta para diltrar una cita por ID
+ *      responses:
+ *          '200':
+ *              description: Respuesta exitosa!
+ *          '404':
+ *              description: Recurso no encontrado
+ */
+
 router.get("/:id", async (req, res) => {
   const result = await citaService.getConsultCitaId(req.params.id);
   if (!result) {
@@ -12,6 +24,13 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  const result = await citaService.getConsultaCita(req.params);
+  if (!result) {
+    res.status(404).json({ error: "No hay datos para mostrar" });
+  } else {
+    res.status(200).send(result);
+  }
+});
 
 module.exports = router;
--- src/services/cita.service.js
+++ src/services/cita.service.js
@@ -21,10 +21,30 @@ const getConsultCitaId = async id => {
 
 const getConsultaCita = async params => {
   const { fecha, medico, propietario } = params;
+  let conn = await sql.connect(sqlConfig.config);
+  try {
+    if (fecha == "" && medico == "" && propietario == "") {
+      let result = await conn.request().query("SELECT * FROM cita");
+    } else {
+      let result = await conn
+        .request()
+        .input("fecha", fecha)
+        .input("medico", medico)
+        .input("propietario", propietario)
+        .query(
+          "SELECT * FROM cita where (@fecha is null or @fecha = cast (cita.fecha as date) and (@medico is null or @medico = id_medico) and (@propietario is null or @propietario = id_propietario))"
+        );
+    }
+    sql.close();
 
-  let result = await conn
-    .request()
-    .query("SELECT * FROM cita where (@fecha is null or @fecha > cita.fecha)");
+    if (result.rowsAffected > 0) {
+      return result;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
 };
 
-module.exports = { getConsultCitaId };
+module.exports = { getConsultCitaId, getConsultaCita };
