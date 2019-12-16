d9654a10fa9fc509ccb1e60ccc01671e3bbb5dc5
estebanl-lagash <estebanl@lagash.com>
2019-12-16 15:45:22 -0300

    cambios cambios

--- src/routes/propietario.routes.js
+++ src/routes/propietario.routes.js
@@ -72,6 +72,7 @@ router.get('/test', function (req, res) {
     sql.connect(sqlConfig.config, function (err) {
         if (err) console.log(err);
         const sqlRequest = new sql.Request();
+<<<<<<< HEAD
 
         sqlRequest.query('select * from persona', function (error, data) {
             if (error) console.log(error);
@@ -112,3 +113,45 @@ router.get('/async', async (req, res) => {
 });
 
 module.exports = router;
+=======
+
+        sqlRequest.query('select * from persona', function (error, data) {
+            if (error) console.log(error);
+            res.send(data);
+            sql.close();
+        })
+    });
+});
+
+//promises
+router.get('/promise', function (req, res) {
+    sql.connect(sqlConfig.config)
+        .then(function (conn) {
+            return conn.query('select * from persona');
+        })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => console.log(error))
+});
+
+//async await
+router.get('/async', async (req, res) => {
+
+    try {
+        let conn = await sql.connect(sqlConfig.config);
+
+        let result = await conn.request().query('select * from persona');
+
+        sql.close();
+
+        res.send(result);
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+});
+
+module.exports = router;
+>>>>>>> parent of 59f8ee8... Actualizacion ejercicio grupal Cita
